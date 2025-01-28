93a06a06c5759ac4fa2de5b39962f927755b5c3f
davidtrujillosm <david.trujillo.sm@gmail.com>
2025-01-27 22:23:39 -0400

    Place_1.0

---
+++ week03/scripts/place-script.js
@@ -0,0 +1,29 @@
+// Display the last modified date for the footer
+const lastModifiedElement = document.getElementById("last-modified");
+const currentYearElement = document.getElementById("year");
+
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = `© ${currentYear} David Trujillo - Cochabamba, Bolivia`;
+
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+
+// Display windchill factor 
+
+let T = 70;
+let vs = 12;
+let windChill;
+
+if (T < 50 && vs > 3) {
+    document.querySelector("#windChill").innerHTML = calculateWincdChill(T, vs);
+} else {
+    document.querySelector("#windChill").innerHTML = "N/A";
+}
+
+
+// Function to calculate windchill
+
+function calculateWincdChill(T, vs) {
+    windChill = 12.12 + (0.6215 * T) - (11.37 * Math.pow(vs, 0.16)) + (0.3965 * T * Math.pow(vs, 0.16));
+    return windChill.toFixed(2)
+}
